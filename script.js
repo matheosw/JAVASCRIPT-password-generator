3ad8006e6418cd2979b3971f18db4960b4399c5d
matheosw <103533878+matheosw@users.noreply.github.com>
2022-05-14 19:59:08 -0700

    added variables

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbols = "!@#$%^&*+-_";
+const p = "";
 
 function generatePassword() {
-  console.log("Button has been clciked")
+let length = prompt("how long would you want the length");
+let uCase = prompt("Would you like Lowercase")
+let lcase = prompt("Would you like Uppercase")
+let number = prompt("Would you like Numbers")
+let symbols = prompt("Would you like Special characters")
+
+  console.log(length, uCase, lCase, number, symbols)
 }
 // Write password to the #password input
 function writePassword() {
